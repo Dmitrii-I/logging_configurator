67201b4b244b4e6ed439fe27d071752e139da971
Dmitrii-I <izgurskii@gmail.com>
2018-10-27 22:08:34 +0200

    Move to own dir

---
+++ file_logger/__init__.py
@@ -0,0 +1,4 @@
+
+from file_logger.core import get_logger
+
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+from file_logger import __version__
+
+
+setup(
+    name='file_logger',
+    version=__version__,
+    description='Simple file logger for Python scripts.',
+    author='Dmitrii Izgurskii',
+    author_email='izgurskii@gmail.com',
+    url='https://github.com/Dmitrii-I/file-logger',
+    license='MIT'
+)
