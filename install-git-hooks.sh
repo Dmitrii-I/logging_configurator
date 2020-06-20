5b471808203137538d36fbe79a7a0f74556317c3
Dmitrii-I <izgurskii@gmail.com>
2020-06-20 19:05:16 +0200

    Fix bad path to target: must be relative wrt hook

--- install-git-hooks.sh
+++ install-git-hooks.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 POSIXLY_CORRECT=1 set -o errexit && set -o nounset && set -o pipefail && unset POSIXLY_CORRECT
-ln -sf pre-push.sh .git/hooks/pre-push
+
+cd .git/hooks
+ln -sf ../../pre-push.sh pre-push
