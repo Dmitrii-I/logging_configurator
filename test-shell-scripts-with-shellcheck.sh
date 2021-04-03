16d877ae418fbb49560876623ad819d6f8843786
Dmitrii-I <izgurskii@gmail.com>
2021-04-03 19:45:19 +0200

    Ignore shellscripts in .venv dir

--- test-shell-scripts-with-shellcheck.sh
+++ test-shell-scripts-with-shellcheck.sh
@@ -3,7 +3,7 @@
 POSIXLY_CORRECT=1 set -o errexit && set -o nounset && set -o pipefail && unset POSIXLY_CORRECT
 
 echo Test shell scripts with shellcheck
-files="$(find -L ./ -iname '*.sh')"
+files="$(find -L ./ -iname '*.sh' -not -path './.venv/*')"
 for f in $files; do
     echo "Test $f with shellcheck"
     shellcheck --external-sources "$f"
